460b6f1624ae952508e92298dd604aa36e353597
Santi Garzon <santiagogarzon04@gmail.com>
2019-03-19 09:38:59 -0300

    fixing shows

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function goToMusic() {
     document.getElementById('fotos').className = ' animated slideOutUp';
     document.getElementById('logo-shows').className = 'animated slideOutUp';
     document.getElementById('shows').className = 'animated slideOutUp';
-    document.getElementById('musica').className = 'show animated slideInUp';
+    document.getElementById('copyrights').className = ' animated bounceOut';
+    document.getElementById('musica').className = 'show animated slideInDown';
 }
 
 function goToFotos() {
@@ -14,6 +15,7 @@ function goToFotos() {
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('logo-shows').className = 'animated slideOutUp';
     document.getElementById('shows').className = 'animated slideOutUp';
+    document.getElementById('copyrights').className = ' animated bounceOut';
     document.getElementById('fotos').className = 'show animated slideInUp';
 }
 
@@ -22,6 +24,25 @@ function goToShows() {
     document.getElementById('logo').className = 'animated slideOutUp';
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('fotos').className = ' animated slideOutUp';
+    document.getElementById('copyrights').className = ' animated bounceOut';
     document.getElementById('logo-shows').className = 'animated slideInUp';
     document.getElementById('shows').className = 'show animated slideInUp';
+}
+
+$(document).ready(loadStuff);
+
+function loadStuff(){
+    $.ajax({
+        type: "POST",
+        url: "path/to/ajax.php",
+        data: { some: data, action: "setContent"},
+        timeout: 1000, //only one second, for a short loading time
+        error: function(){ 
+            console.log("An error occured. The div will reload.");
+            loadStuff(); 
+        },
+        success: function(){
+            $("#divid").load("path/to/template.php"); //div gets filled with template
+        }
+    });
 }
