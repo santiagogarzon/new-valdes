edb615db5aa92e70b3c39ff466313dcab62a1636
Santi Garzon <santiagogarzon04@gmail.com>
2019-04-08 09:11:07 -0300

    adding nosotros

--- main.js
+++ main.js
@@ -1,32 +1,87 @@
 
 function goToMusic() {
     window.scrollTo(0, 0);
+    document.getElementById('social-media').className = 'show';
+    document.getElementById('social-media-nosotros').className = 'not-show';   
+    document.getElementById('nosotrosLogo').className = 'animated slideOutUp';
+    document.getElementById('normalLogo').className = 'animated slideInUp';
+    document.getElementById('links').className = 'desktop';
+    document.getElementById('musica').className = 'show animated slideInUp';
     document.getElementById('logo').className = 'animated slideOutUp';
     document.getElementById('fotos').className = ' animated slideOutUp';
     document.getElementById('logo-shows').className = 'animated slideOutUp';
     document.getElementById('shows').className = 'animated slideOutUp';
     document.getElementById('copyrights').className = ' animated bounceOut';
-    document.getElementById('musica').className = 'show animated slideInDown';
+    document.getElementById('nosotros').className = 'animated slideOutUp';
+    document.getElementById('videos').className = 'animated slideOutUp';
+}
+
+function goToVideos() {
+  window.scrollTo(0, 0);
+  document.getElementById('social-media').className = 'show';
+  document.getElementById('social-media-nosotros').className = 'not-show';   
+  document.getElementById('nosotrosLogo').className = 'not-shown';
+  document.getElementById('normalLogo').className = 'shown';
+  document.getElementById('links').className = 'desktop';
+  document.getElementById('videos').className = 'show animated slideInUp';
+  document.getElementById('logo').className = 'show animated slideInUp';
+  document.getElementById('fotos').className = ' animated slideOutUp';
+  document.getElementById('logo-shows').className = 'animated slideOutUp';
+  document.getElementById('shows').className = 'animated slideOutUp';
+  document.getElementById('copyrights').className = ' animated bounceOut';
+  document.getElementById('nosotros').className = 'animated slideOutUp';
+  document.getElementById('musica').className = 'animated slideOutUp';
 }
 
 function goToFotos() {
     window.scrollTo(0, 0);
+    document.getElementById('social-media').className = 'show';
+    document.getElementById('social-media-nosotros').className = 'not-show';   
+    document.getElementById('nosotrosLogo').className = 'not-shown';
+    document.getElementById('normalLogo').className = 'shown';
+    document.getElementById('links').className = 'desktop';
     document.getElementById('logo').className = 'animated slideOutUp';
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('logo-shows').className = 'animated slideOutUp';
     document.getElementById('shows').className = 'animated slideOutUp';
     document.getElementById('copyrights').className = ' animated bounceOut';
     document.getElementById('fotos').className = 'show animated slideInUp';
+    document.getElementById('nosotros').className = 'animated slideOutUp';
+    document.getElementById('videos').className = 'animated slideOutUp';
 }
 
 function goToShows() {
     window.scrollTo(0, 0);
+    document.getElementById('social-media').className = 'show';
+    document.getElementById('social-media-nosotros').className = 'not-show';    
+    document.getElementById('nosotrosLogo').className = 'not-shown';
+    document.getElementById('normalLogo').className = 'shown';
+    document.getElementById('links').className = 'desktop';
     document.getElementById('logo').className = 'animated slideOutUp';
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('fotos').className = ' animated slideOutUp';
     document.getElementById('copyrights').className = ' animated bounceOut';
     document.getElementById('logo-shows').className = 'animated slideInUp';
     document.getElementById('shows').className = 'show animated slideInUp';
+    document.getElementById('nosotros').className = 'animated slideOutUp';
+    document.getElementById('videos').className = 'animated slideOutUp';
+}
+
+function goToNosotros() {
+  window.scrollTo(0, 0);
+  document.getElementById('social-media').className = 'not-show';
+  document.getElementById('social-media-nosotros').className = 'show';
+  document.getElementById('logo').className = 'animated slideInUp';
+  document.getElementById('nosotrosLogo').className = 'shown';
+  document.getElementById('normalLogo').className = 'not-shown';
+  document.getElementById('links').className = 'desktop links-negros';
+  document.getElementById('musica').className = 'animated slideOutUp';
+  document.getElementById('fotos').className = ' animated slideOutUp';
+  document.getElementById('shows').className = 'animated slideOutUp';
+  document.getElementById('copyrights').className = ' animated bounceOut';
+  document.getElementById('logo-shows').className = 'animated slideOutUp';
+  document.getElementById('nosotros').className = 'show animated slideInUp';
+  document.getElementById('videos').className = 'animated slideOutUp';
 }
 
 function goToMusicM() {
@@ -81,6 +136,8 @@ function goBackToLink() {
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('mobile-close-icon').className = 'animated bounceOut';
 }
+
+
 
 //$(document).ready();
 
