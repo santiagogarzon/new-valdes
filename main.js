1a773dd40dd7032a68acc546ed89318998d4413b
Santi Garzon <santiagogarzon04@gmail.com>
2019-04-21 22:13:57 -0300

    adding canvas

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 screen.orientation.lock("portrait-primary");
 
+function goToHome() {
+    window.scrollTo(0, 0);
+    document.getElementById('social-media').className = 'show';
+    document.getElementById('social-media-nosotros').className = 'not-show';   
+    document.getElementById('nosotrosLogo').className = 'not-shown';
+    document.getElementById('normalLogo').className = 'not-shown';
+    document.getElementById('links').className = 'desktop';
+    document.getElementById('musica').className = 'animated slideOutUp';
+    document.getElementById('logo').className = 'show animated slideInUp';
+    document.getElementById('fotos').className = ' animated slideOutUp';
+    document.getElementById('logo-shows').className = 'not-shown';
+    document.getElementById('shows').className = 'animated slideOutUp';
+    document.getElementById('copyrights').className = ' ';
+    document.getElementById('nosotros').className = 'animated slideOutUp';
+    document.getElementById('videos').className = 'animated slideOutUp';
+
+}
+
 function goToMusic() {
     window.scrollTo(0, 0);
     document.getElementById('social-media').className = 'show';
@@ -12,7 +30,7 @@ function goToMusic() {
     document.getElementById('fotos').className = ' animated slideOutUp';
     document.getElementById('logo-shows').className = 'not-shown';
     document.getElementById('shows').className = 'animated slideOutUp';
-    document.getElementById('copyrights').className = ' animated bounceOut';
+    document.getElementById('copyrights').className = ' ';
     document.getElementById('nosotros').className = 'animated slideOutUp';
     document.getElementById('videos').className = 'animated slideOutUp';
 }
@@ -29,7 +47,7 @@ function goToVideos() {
   document.getElementById('fotos').className = ' animated slideOutUp';
   document.getElementById('logo-shows').className = 'animated slideOutUp';
   document.getElementById('shows').className = 'animated slideOutUp';
-  document.getElementById('copyrights').className = ' animated bounceOut';
+  document.getElementById('copyrights').className = ' ';
   document.getElementById('nosotros').className = 'animated slideOutUp';
   document.getElementById('musica').className = 'animated slideOutUp';
 }
@@ -45,7 +63,7 @@ function goToFotos() {
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('logo-shows').className = 'animated slideOutUp';
     document.getElementById('shows').className = 'animated slideOutUp';
-    document.getElementById('copyrights').className = ' animated bounceOut';
+    document.getElementById('copyrights').className = ' ';
     document.getElementById('fotos').className = 'show animated slideInUp';
     document.getElementById('nosotros').className = 'animated slideOutUp';
     document.getElementById('videos').className = 'animated slideOutUp';
@@ -61,7 +79,7 @@ function goToShows() {
     document.getElementById('logo').className = 'animated slideOutUp';
     document.getElementById('musica').className = 'animated slideOutUp';
     document.getElementById('fotos').className = ' animated slideOutUp';
-    document.getElementById('copyrights').className = ' animated bounceOut';
+    document.getElementById('copyrights').className = ' ';
     document.getElementById('logo-shows').className = 'animated slideInUp';
     document.getElementById('shows').className = 'show animated slideInUp';
     document.getElementById('nosotros').className = 'animated slideOutUp';
@@ -79,7 +97,7 @@ function goToNosotros() {
   document.getElementById('musica').className = 'animated slideOutUp';
   document.getElementById('fotos').className = ' animated slideOutUp';
   document.getElementById('shows').className = 'animated slideOutUp';
-  document.getElementById('copyrights').className = ' animated bounceOut';
+  document.getElementById('copyrights').className = ' ';
   document.getElementById('logo-shows').className = 'animated slideOutUp';
   document.getElementById('nosotros').className = 'show animated slideInUp';
   document.getElementById('videos').className = 'animated slideOutUp';
@@ -156,7 +174,7 @@ function goToNosotrosM() {
     document.getElementById('shows').className = ' animated slideOutUp';
     document.getElementById('videos').className = 'animated slideOutUp';
     document.getElementById('nosotros-mobile').className = 'show animated slideInUp';
-    document.getElementById('mobile-close-icon').className = 'show animated bounceIn black';
+    document.getElementById('mobile-close-icon').className = 'show animated bounceIn black nosotros';
 }
 
 function goBackToLink() {
@@ -298,3 +316,110 @@ $.ajax(settings).done(function (response) {
         }
     });
 }*/
+
+let OPT = {
+    selector: "#sparks",
+    amount: 5000,
+    speed: 0.05, // pixels per frame
+    lifetime: 50,
+    direction: { x: -0.5, y: 1 },
+    size: [2, 2],
+    maxopacity: 1,
+    color: '233,34,0',
+    randColor: false,
+    acceleration: [5, 40] };
+  
+  
+  if (window.innerWidth < 520) {
+    OPT.speed = 0.05;
+    OPT.color = '233,34,0';
+  }
+  
+  (function spark() {
+    const canvas = document.querySelector(OPT.selector);
+    const ctx = canvas.getContext("2d");
+  
+    let sparks = [];
+  
+    window.addEventListener('resize', () => {
+      setCanvasWidth();
+    });
+  
+    function setCanvasWidth() {
+      ctx.canvas.width = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
+    }
+  
+    function rand(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  
+    // Init animation
+    function init() {
+      setCanvasWidth();
+  
+      window.setInterval(() => {
+        if (sparks.length < OPT.amount) {
+          addSpark();
+        }
+      }, 1000 / OPT.amount);
+  
+      window.requestAnimationFrame(draw);
+    }
+  
+    function draw() {
+      ctx.fillStyle = 'rgba(0,0,0, 0.1)';
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  
+      sparks.forEach((spark, i, array) => {
+  
+        if (spark.opacity <= 0) {
+          array.splice(i, 1);
+        } else {
+          drawSpark(spark);
+        }
+  
+      });
+  
+      window.requestAnimationFrame(draw);
+    }
+  
+    function Spark(x, y) {
+      this.x = x;
+      this.y = y;
+      this.age = 0;
+      this.acceleration = rand(OPT.acceleration[0], OPT.acceleration[1]);
+  
+      this.color = OPT.randColor ?
+      rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) :
+      OPT.color;
+  
+      this.opacity = OPT.maxopacity - this.age / (OPT.lifetime * rand(1, 10));
+  
+      this.go = function () {
+        this.x += OPT.speed * OPT.direction.x * this.acceleration / 2;
+        this.y += OPT.speed * OPT.direction.y * this.acceleration / 2;
+  
+        this.opacity = OPT.maxopacity - ++this.age / OPT.lifetime;
+      };
+    }
+  
+    function addSpark() {
+      let x = rand(-200, window.innerWidth + 200);
+      let y = rand(-200, window.innerHeight + 200);
+      sparks.push(new Spark(x, y));
+    }
+  
+    function drawSpark(spark) {
+      let x = spark.x,y = spark.y;
+  
+      spark.go();
+  
+      ctx.beginPath();
+      ctx.fillStyle = `rgba(${spark.color}, ${spark.opacity})`;
+      ctx.rect(x, y, OPT.size[0], OPT.size[1], 0, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  
+    init();
+  })();
